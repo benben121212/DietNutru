a0dcddb8b5a58d540a003d668a2137cd72ddc8af
benben121212 <benoitdelpontnew@gmail.com>
2023-05-19 16:38:23 +0200

    images

--- components/Presentation.js
+++ components/Presentation.js
@@ -14,7 +14,7 @@ class Presentation {
             <h1>Présentation</h1>
             <p class="lead mt-4">Sandrine Coupart est une diététicienne-nutritionniste dont le cabinet est situé à Caen. En tant que professionnelle de santé, elle prend en charge des patients dans le cadre de consultations diététiques. Madame Coupart anime également des ateliers de prévention et d’information sur la nutrition.</p>
             <p>Son fonctionnement habituel était de transmettre par email à ses patients des recettes santé. N’ayant pas de site web, elle voulait profiter de l’occasion pour partager, à l’avenir, quelques-unes de ses recettes à un plus grand nombre de visiteurs. De plus, madame Coupart désire qu’il y ait un système d’authentification sur son site, afin de proposer pour chaque patient des recettes supplémentaires adaptées à son régime.</p>
-            <img src="/images/dieteticienne_nutritionniste.jpg" alt="Image de présentation" class="img-fluid">
+            <img src="/images/dieteticienne_nutritionniste.png" alt="Image de présentation" class="img-fluid">
           </div>
         </div>
       `;
